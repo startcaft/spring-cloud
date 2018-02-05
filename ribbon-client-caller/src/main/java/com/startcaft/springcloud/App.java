24ebe0c02ca50a42b7be7d711676d123d112873a
startcaft <51418996@qq.com>
2018-02-05 22:03:44 +0800

    Ribbon负载均衡的基本机制。一个负载均衡器，管理服务器列表，配置负载均衡策略获取服务器

--- ribbon-client-caller/src/main/java/com/startcaft/springcloud/App.java
+++ ribbon-client-caller/src/main/java/com/startcaft/springcloud/App.java
@@ -1,6 +1,14 @@
 package com.startcaft.springcloud;
 
 
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author pikai
  */
@@ -15,5 +23,23 @@ public class App {
 //            String json = response.getEntity(String.class);
 //            System.out.println(json);
 //        }
+
+        // Ribbon 负载均衡的基本机制
+        BaseLoadBalancer loadBalancer = new BaseLoadBalancer();
+
+        // 设置服务器列表
+        List<Server> serverList = new ArrayList<>();
+        serverList.add(new Server("http://localhost",8080));
+        serverList.add(new Server("http://localhost",8081));
+        loadBalancer.addServers(serverList);
+
+        // 负载均衡策略
+        IRule rule = new RoundRobinRule();
+        loadBalancer.setRule(rule);
+
+        for (int i=0;i<20;i++){
+            Server currentServer = loadBalancer.chooseServer();
+            System.out.println(currentServer.getHost() + ":" + currentServer.getPort());
+        }
     }
 }
