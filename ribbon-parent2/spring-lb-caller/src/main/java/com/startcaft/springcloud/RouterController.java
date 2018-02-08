ddb72a9cc71197a3590ebe86bb6880d6aff64eef
startcaft <51418996@qq.com>
2018-02-08 15:42:06 +0800

    SpringCloud中使用Ribbon的几种方式

--- ribbon-parent2/spring-lb-caller/src/main/java/com/startcaft/springcloud/RouterController.java
+++ ribbon-parent2/spring-lb-caller/src/main/java/com/startcaft/springcloud/RouterController.java
@@ -1,6 +1,11 @@
 package com.startcaft.springcloud;
 
+import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +31,23 @@ public class RouterController {
         return result;
     }
 
+
+    @Autowired
+    private LoadBalancerClient client;
+
+    @RequestMapping("/lb")
+    public ServiceInstance lb(){
+        ServiceInstance serviceInstance = client.choose("server-lb-provider");
+        return serviceInstance;
+    }
+
+    @Autowired
+    private SpringClientFactory factory;
+
+    @RequestMapping("/factory")
+    public String factory(){
+        // 获取默认的 负载均衡器
+        ZoneAwareLoadBalancer loadBalancer = (ZoneAwareLoadBalancer) factory.getLoadBalancer("server-lb-provider");
+        return "默认策略:" +  loadBalancer.getRule().getClass().getName();
+    }
 }
